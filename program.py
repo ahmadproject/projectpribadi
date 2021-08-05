36582f7c5df8252e841cddd8e345627dfcfd2524
MOH HOSEN A-19 <hoseinsingo17@gmail.com>
2021-08-05 20:50:01 +0700

    first commit

---
+++ program.py
@@ -0,0 +1,58 @@
+print("+==============================================================+")
+print("+========SELAMAT DATANG DI SISTEM PROGRAM PRIBADI SAYA=========+")
+print("+==================MOH.HOSEN BISAKOM===========================+")
+print("+==============================================================+")
+nama = input("NAMA\t: ")
+pilihan = input(("halo.........."+nama+",\napakah kamu mahasiswa uim ? (y/n :"))
+
+print("@@@@@@@@\kalu kamu mahasiswa uim silahkan/@@@@@@@@@@@")
+print ("+===============MASUKKAN DATA ANDA=================+")
+print ("+==================================================+")
+a = nama = input ("NAMA :")
+b = npm = input ("NPM : ")
+c = prodi = input ("PRODI: ")
+d = semester = input ("SEMESTER :")
+e = angkatan = input ("ANGKATAN :")
+print ("+==================================================+")
+
+print ("+==================================================+")
+print(("|")+("       ")+"NAMA :"+("  ")+a+("         ")+("|"))
+print(("|")+("       ")+"NPM  :"+("  ")+b+("         ")+("|"))
+print(("|")+("       ")+"PRODI:"+("  ")+c+("         ")+("|"))
+print(("|")+("       ")+"SEMESTER:"+(" ")+d+("      ")+("|"))
+print ("+==================================================+")
+
+print("#++++++++++++++DATA ANDA SUDAH BENAR++++++++++++++++#")
+print("+========SILAHKAN MASUKKAN NILAI UAS ANDA===========+")
+nilai = int(input("masukkan nilai anda : "))
+if(nilai>80):
+    print("nilai anda [A] ")
+else:
+    if(nilai > 70):
+        print("nilai anda [B] ")
+    else:
+        if(nilai > 60):
+            print("nilai anda [C]")
+        else:
+            print("nilai anda [E]")
+print("+=============DATA ANDA SUDAH BERHASIL KAMI INPUT===============+")
+
+pilihan = input (",\njika mau lanjut ketik  (y) :")
+print("\n+===============MASUKKAN DATA ANDA LAGI=================+?")
+a = nama = input ("NAMA :")
+b = npm = input ("NPM : ")
+c = prodi = input ("PRODI: ")
+d = semester = input ("SEMESTER :")
+e = angkatan = input ("ANGKATAN :")
+print ("+==================================================+")
+
+print ("+==================================================+")
+print(("|")+("       ")+"NAMA :"+("  ")+a+("         ")+("|"))
+print(("|")+("       ")+"NPM  :"+("  ")+b+("         ")+("|"))
+print(("|")+("       ")+"PRODI:"+("  ")+c+("         ")+("|"))
+print(("|")+("       ")+"SEMESTER:"+(" ")+d+("      ")+("|"))
+print ("+==================================================+")
+
+
+
+
